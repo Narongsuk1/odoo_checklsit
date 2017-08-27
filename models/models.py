17f7b14aa6342d5f4a2611d39facd2e299b88b72
Narongsak Keawmanee <klogic@hotmail.co.th>
2017-08-27 21:57:02 +0700

    remove not useage code

--- models/models.py
+++ models/models.py
@@ -8,10 +8,3 @@ class checklist(models.Model):
     description = fields.Text("Description")
     complete = fields.Boolean("Complete",help="status of this task. are u finish?")
     _order = "id asc" 
-
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
